9a745e7ea0254cd2db3dd94309de6f8b2eade8aa
Nuri Ulgen <nuriulgen04400@gmail.com>
2022-11-17 22:01:59 +0300

    created context extension.

--- lib/core/constants/extension/context_extension.dart
+++ lib/core/constants/extension/context_extension.dart
@@ -42,15 +42,16 @@ extension ContextExtension on BuildContext {
   double get highValue => height * 0.1;
 
   // HEIGHT- WIDTH
+  double get hw5=> 10;
   double get hw10 => 10;
   double get hw20 => 20;
+  double get hw25 => 25;
   double get hw50 => 50;
+  double get hw70 => 70;
   double get hw100 => 100;
   double get hw150 => 150;
   double get hw200 => 200;
 
   //Count
-  int get onBoardPageCount => 3;
-  int get tabBarCount => 2;
-  int get initialIndex => 0;
+  int get crossAxisCountValue => 2;
 }
