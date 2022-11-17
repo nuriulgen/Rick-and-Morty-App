dfecb1546daa2a347e69d5afbbc542e1876d935e
Nuri Ulgen <nuriulgen04400@gmail.com>
2022-11-17 22:01:45 +0300

    created color extension.

--- lib/core/constants/extension/color_extension.dart
+++ lib/core/constants/extension/color_extension.dart
@@ -3,10 +3,9 @@ import 'package:flutter/material.dart';
 extension ColorExtension on BuildContext {
   Color get white => Colors.white;
   Color get black => Colors.black;
+  Color get green => Colors.green;
 
-  Color get chasm => const Color(0xFF8F5F43);
   Color get heatherGrey => const Color(0xFF9D9EA3);
-  Color get blackChesnut => const Color(0xFF272422);
 
   Color get mediumBlack => const Color.fromARGB(144, 0, 0, 0);
 }
