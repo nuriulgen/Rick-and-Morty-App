9c335fe6743f5f14091f49871b8b1606c533de2a
Nuri Ulgen <nuriulgen04400@gmail.com>
2022-11-17 22:02:25 +0300

    created string extension.

--- lib/core/constants/extension/string_extension.dart
+++ lib/core/constants/extension/string_extension.dart
@@ -8,6 +8,8 @@ class AppStringConstants {
   AppStringConstants._init();
 
   //STRING
-  final String characterTitle  = 'Characters';
-  
+  final String characterTitle = 'Characters';
+  final String characterDetailTitle = 'Characters Detail';
+  final String lastLocationText = 'Last Location:';
+  final String episodeTitle = 'Episode List';
 }
